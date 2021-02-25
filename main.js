681a21cde11e239314f56acc6de9ff57d8f2bdf4
Nata <nata@sapozhkov.net>
2021-02-25 12:02:47 +0100

    New title and comma

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 gtag('event', 'page_view', {
     page_title: '<Page Title>',
     page_location: '<Page Location>',
-    page_path: '<Page Path>'
+    page_path: '<Page Path>',
     send_to: '<GA_MEASUREMENT_ID>'
   })
