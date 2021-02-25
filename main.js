ec37cd7481374398e4c3bdfe0994c88936137c3c
Nata <nata@sapozhkov.net>
2021-02-25 11:56:39 +0100

    gtag event pv

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-gtag('config', 'G-759YDBZZPV');
+gtag('event', 'page_view', {
+    page_title: '<Page Title>',
+    page_location: '<Page Location>',
+    page_path: '<Page Path>'
+    send_to: '<GA_MEASUREMENT_ID>'
+  })
